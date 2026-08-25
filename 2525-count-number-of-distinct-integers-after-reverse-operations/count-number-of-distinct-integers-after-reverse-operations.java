6c42d9c8a49aef0b9c336619be2faaaa174e786f
Himadri Bera <hbera5753@gmail.com>
2026-08-25 22:19:56 +0530

    Time: 57 ms (65.81%) | Memory: 105.5 MB (73.93%) - LeetSync

---
+++ 2525-count-number-of-distinct-integers-after-reverse-operations/count-number-of-distinct-integers-after-reverse-operations.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int countDistinctIntegers(int[] nums) {
+        Set<Integer> B = new HashSet<>();
+
+        for (int c : nums) {
+            int C = reverse(c);
+            B.add(c);
+            B.add(C);
+        }
+
+        return B.size();
+    }
+
+    private int reverse(int num) {
+        int rev = 0;
+        while (num > 0) {
+            rev = rev * 10 + (num % 10);
+            num /= 10;
+        }
+        return rev;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
