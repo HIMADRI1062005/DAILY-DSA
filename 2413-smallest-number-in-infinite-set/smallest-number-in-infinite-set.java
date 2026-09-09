5fefa5d5d60674b52301164fb4a454f2cf1b61ae
Himadri Bera <hbera5753@gmail.com>
2026-09-09 20:56:47 +0530

    Time: 10 ms (88.40%) | Memory: 47.3 MB (55.35%) - LeetSync

---
+++ 2413-smallest-number-in-infinite-set/smallest-number-in-infinite-set.java
@@ -0,0 +1,34 @@
+class SmallestInfiniteSet {
+    TreeSet<Integer> set;
+    int currentCounter;
+    public SmallestInfiniteSet() {
+        set = new TreeSet<Integer>();
+        currentCounter = 1;
+    }
+    
+    public int popSmallest() {
+        if(!set.isEmpty()){
+            int top = set.first();
+            set.remove(top);
+            return top;
+        }
+        return currentCounter++;
+    }
+    
+    public void addBack(int num) {
+        if(num >= currentCounter)
+            return;
+        set.add(num);
+    }
+}
+
+/**
+ * Your SmallestInfiniteSet object will be instantiated and called as such:
+ * SmallestInfiniteSet obj = new SmallestInfiniteSet();
+ * int param_1 = obj.popSmallest();
+ * obj.addBack(num);
+ */
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
