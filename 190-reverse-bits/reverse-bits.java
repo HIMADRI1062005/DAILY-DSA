7d17cf46eeb1905ec2f16bea16398fe313bb3624
Himadri Bera <hbera5753@gmail.com>
2026-09-10 22:30:01 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (18.22%) - LeetSync

---
+++ 190-reverse-bits/reverse-bits.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int reverseBits(int n) {
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            res <<= 1;
+            res |= (n & 1);
+            n >>>= 1;
+        }
+        return res;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
